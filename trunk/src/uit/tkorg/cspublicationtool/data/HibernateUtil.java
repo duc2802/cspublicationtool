dc13a3ce6fad7f5158d129a39bae36ad1f8e6705
tiendo.vn@gmail.com <tiendo.vn@gmail.com@e7245cfb-2c84-49a3-6c70-a5728351e23f>
2011-11-05 17:10:26 +0000

    Add  BO

---
+++ trunk/src/uit/tkorg/cspublicationtool/data/HibernateUtil.java
@@ -0,0 +1,78 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package uit.tkorg.cspublicationtool.data;
+
+
+import org.hibernate.cfg.*;
+import org.hibernate.*;
+import org.hibernate.Session;
+import org.hibernate.criterion.*;
+
+/**
+ *
+ * @author tiendv
+ */
+public class HibernateUtil {
+
+    private SessionFactory sessionFactory;
+    private Session session;
+    private String sessionFactoryConfigPath;
+
+    protected LockMode lockMode;
+    protected Order order;
+    protected Criteria criteria;
+    protected Restrictions restrictions;
+
+    public HibernateUtil() throws Exception {
+        sessionFactoryConfigPath = "";
+        sessionFactory = new Configuration().configure().buildSessionFactory();
+    }
+
+    public HibernateUtil(String sessionFactoryConfigPath) {
+        this.sessionFactoryConfigPath = sessionFactoryConfigPath;
+        sessionFactory = new Configuration().configure(sessionFactoryConfigPath).buildSessionFactory();
+    }
+
+    /**
+     * Begin a transaction
+     */
+    protected void beginTransaction() {
+        session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+    }
+
+    /**
+     * Commit transaction and close session
+     */
+    protected void commitAndClose() {
+        if (session != null) {
+            session.getTransaction().commit();
+            if (session.isOpen()) {
+                session.close();
+            }
+        }
+    }
+
+    /**
+     * Get Current Session
+     * @return session
+     * @throws Exception
+     */
+    protected Session getCurrentSession() throws Exception {
+        if (session == null) { // check session null
+            if (sessionFactory == null) { // buil Factory Session if it's null
+                if (sessionFactoryConfigPath == null || sessionFactoryConfigPath.equals("")) {
+                    sessionFactory = new Configuration().configure().buildSessionFactory();
+                } else {
+                    sessionFactory = new Configuration().configure(this.sessionFactoryConfigPath).buildSessionFactory();
+                }
+            }
+            session = sessionFactory.getCurrentSession();
+        }
+
+        return session;
+    }
+}
