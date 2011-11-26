7a9b011b70f3a64d9ca2050cc274a2eb8de4d70a
duc2802@gmail.com <duc2802@gmail.com@e7245cfb-2c84-49a3-6c70-a5728351e23f>
2011-11-26 16:54:47 +0000

    Generation SQL

--- trunk/src/uit/tkorg/cspublicationtool/main/CSPublicationParser.java
+++ trunk/src/uit/tkorg/cspublicationtool/main/CSPublicationParser.java
@@ -16,14 +16,10 @@ import javax.xml.parsers.SAXParserFactory;
 public class CSPublicationParser {
 
     public CSPublicationParser(String uri) {
-        
-
         try {
             long startTime = System.currentTimeMillis();
-
             SAXParserFactory parserFactory = SAXParserFactory.newInstance();
 	    SAXParser parser = parserFactory.newSAXParser();
-            
 	    CSPublicationSAXEventHandler handler = new CSPublicationSAXEventHandler();  
             parser.getXMLReader().setFeature("http://xml.org/sax/features/validation", true);
             parser.parse(new File(uri), handler);     
