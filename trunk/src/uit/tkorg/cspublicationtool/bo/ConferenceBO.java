cabcb31c173cc4ca44b50eb234987055b3e91c35
tiendo.vn <tiendo.vn@e7245cfb-2c84-49a3-6c70-a5728351e23f>
2011-12-05 02:49:58 +0000

    Last update tiendv

--- trunk/src/uit/tkorg/cspublicationtool/bo/ConferenceBO.java
+++ trunk/src/uit/tkorg/cspublicationtool/bo/ConferenceBO.java
@@ -4,6 +4,9 @@
  */
 package uit.tkorg.cspublicationtool.bo;
 
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import uit.tkorg.cspublicationtool.data.ManagerBase;
 import uit.tkorg.cspublicationtool.entities.*;
 
@@ -24,6 +27,24 @@ public class ConferenceBO extends ManagerBase<Conference> {
               conferenceBO = new ConferenceBO();
           }
          return conferenceBO;
+    }
+     
+     
+      public Conference checkExitConference(String nameConference)
+    {
+        
+       String query = "conferenceName='" + nameConference +"'";
+       List<Conference> list;
+        try {
+            list = getBySQLQuery(query, null, 0);
+            if(list.isEmpty())
+                return null;
+            else
+                return list.get(0);         
+        } catch (Exception ex) {
+            Logger.getLogger(Conference.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
     }
     
 }
