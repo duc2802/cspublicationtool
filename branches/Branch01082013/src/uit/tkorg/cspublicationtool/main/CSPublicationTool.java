7de6297157aa22abf03724255e7f1984356877d4
daolavi@gmail.com <daolavi@gmail.com@e7245cfb-2c84-49a3-6c70-a5728351e23f>
2013-08-29 19:54:39 +0000

    + convert xml to dblp

--- branches/Branch01082013/src/uit/tkorg/cspublicationtool/main/CSPublicationTool.java
+++ branches/Branch01082013/src/uit/tkorg/cspublicationtool/main/CSPublicationTool.java
@@ -16,24 +16,24 @@ public class CSPublicationTool {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws FileNotFoundException {     
-//        String[] listPath = new String[]
-//        {
-//          "C://Data//dblp01.xml",
-//          "C://Data//dblp02.xml",
-//          "C://Data//dblp03.xml",
-//          "C://Data//dblp04.xml",
-//          "C://Data//dblp05.xml",
-//          "C://Data//dblp06.xml",
-//          "C://Data//dblp07.xml",
-//          "C://Data//dblp08.xml",
-//          "C://Data//dblp09.xml",
-//          "C://Data//dblp10.xml",
-//        };
         String[] listPath = new String[]
         {
-            "C://Data//temp.xml",
-            "C://Data//temp2.xml",
+          "C://Data//dblp01.xml",
+          "C://Data//dblp02.xml",
+          "C://Data//dblp03.xml",
+          "C://Data//dblp04.xml",
+          "C://Data//dblp05.xml",
+          "C://Data//dblp06.xml",
+          "C://Data//dblp07.xml",
+          "C://Data//dblp08.xml",
+          "C://Data//dblp09.xml",
+          "C://Data//dblp10.xml",
         };
+//        String[] listPath = new String[]
+//        {
+//            "C://Data//temp.xml",
+//            "C://Data//temp2.xml",
+//        };
         CSPublicationParser p = new CSPublicationParser(listPath) ; 
     }
 }
